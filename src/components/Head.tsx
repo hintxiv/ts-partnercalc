cc52588c0ab52aefac77ae67e3c42697bff95844
hintxiv <65056303+hintxiv@users.noreply.github.com>
2022-11-23 13:59:38 -0500

    move meta tags to index.html

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -7,13 +7,5 @@ export function Head() {
 
     return <Helmet>
         <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta property="description" content="A tool for analyzing Dance Partner damage contribution in Final Fantasy XIV (FFXIV)." />
-        <meta property="og:description" content="A tool for analyzing Dance Partner damage contribution in Final Fantasy XIV (FFXIV)." />
-        <meta property="og:title" content="partnercalc" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="favicon.ico" />
-        <meta property="og:url" content="https://partnercalc.app" />
-        <link rel="canonical" href="https://partnercalc.app" />
     </Helmet>
 }
