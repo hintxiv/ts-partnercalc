11af7dd89820dbf3800a9a19de93d36df3e1bdc5
hintxiv <65056303+hintxiv@users.noreply.github.com>
2025-05-24 15:44:54 -0400

    Upgrade react-dom to 18 and workflows to actions/cache@v4 (#51)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('output'))
+const container = document.getElementById('output')
+const root = createRoot(container)
+root.render(<BrowserRouter><App /></BrowserRouter>)
